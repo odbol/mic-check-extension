f028083c8a4f82c9dac3ee3ed4723113fbeec603
Tyler Freeman <fuego@google.com>
2022-10-17 14:30:42 -0700

    feat: styling, add logos and mic on/off message 

--- src/pages/Newtab/Newtab.tsx
+++ src/pages/Newtab/Newtab.tsx
@@ -1,32 +1,39 @@
 import React, {useEffect, useState} from 'react';
 // @ts-ignore
-import logo from '../../assets/img/logo.svg';
+import ic_mic from '../../assets/img/ic_mic.svg';
+// @ts-ignore
+import ic_mic_off from '../../assets/img/ic_mic_off.svg';
+
 import {AudioDevices} from '../Options/AudioDevices';
 import {MediaDeviceId} from '../Options/FavoriteDevices';
 import {checkForUnavailableDevices} from '../Options/FavoritesChecker';
 import './Newtab.css';
 import './Newtab.scss';
 
-const DeviceChecker = () => {
+interface IDeviceCheckerProps {
+  onAvailablility: (isAvaialable: boolean) => void;
+}
+
+const DeviceChecker = (props: IDeviceCheckerProps) => {
   const [unavailbleFavs, setUnavailbleFavs] = useState<Array<MediaDeviceId>>([]);
 
   useEffect(() => {
     checkForUnavailableDevices()
       .then(unavailableFavs => {
         setUnavailbleFavs(unavailableFavs);
+
+        props.onAvailablility(unavailableFavs.length === 0);
       })
   }, []);
 
-  if (unavailbleFavs.length === 0) {
-    return (
-      <p>All devices acccounted for!</p>
-    )
-  }
-
   return (
-    <div className="deviceChecker">
-      Ooops, can't find your favorite AV device {unavailbleFavs[0]}. Is it plugged in?
-    </div>
+    <p className="deviceChecker">
+      {unavailbleFavs.length > 0 ?
+        <span>Ooops, can't find your favorite AV device <span className='deviceName'>{unavailbleFavs[0]}.</span> Is it plugged in?</span>
+        :
+        <span>All AV devices acccounted for!</span>
+      }
+    </p>
   );
 };
 
@@ -35,6 +42,7 @@ const Newtab = () => {
   const audioDevices = new AudioDevices();
   const [devices, setDevices] = useState<Array<MediaDeviceInfo>>([]);
   const [error, setError] = useState<Error|null>(null);
+  const [isAvailable, setIsAvailable] = useState(true);
 
   const loadDevices = () => {
     audioDevices.getDevices()
@@ -45,16 +53,19 @@ const Newtab = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
+        <img src={isAvailable ? ic_mic : ic_mic_off} className="App-logo" alt="logo" />
+
+        <DeviceChecker
+          onAvailablility={setIsAvailable} />
+      </header>
+      <div>
+        <button onClick={loadDevices}>Choose favorite devices</button>
 
         <button onClick={loadDevices}>Refresh devices</button>
-
-        <DeviceChecker />
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+        <p>
+          Mic check: checks that your favorite microphone or camera is plugged in and detected.
+        </p>
+      </div>
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -145,7 +145,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-128.png',
+          from: 'src/assets/img/ic_mic_48px.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
@@ -154,7 +154,7 @@ var options = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: 'src/assets/img/icon-34.png',
+          from: 'src/assets/img/ic_mic_128px.png',
           to: path.join(__dirname, 'build'),
           force: true,
         },
